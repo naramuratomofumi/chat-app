f7809bb8fde249c102bacd8e6e00a90409a576f4
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-24 17:07:47 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,19 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create #配列として受け取ったデータを含むストロングパラメーターを定義
+    @room = Room.new(room_params)
+    if @room.save            #保存が成功したかどうかで処理を分岐
+      redirect_to root_path  #redirect_toメソッドでルートパスにリダイレクト
+    else
+      render :new #renderメソッドでrooms/new.html.erbのページを表示
+    end
+  end
+
+  private
+
+  def room_params #ストロングパラメーター
+    params.require(:room).permit(:name, user_ids: []) #配列に対して保存を許可したい場合は、キーに対し[]を値として記述
+  end
 end
