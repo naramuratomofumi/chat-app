6c8630b350a139c00f0c0526e1ee8d08d6e2f429
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-24 17:23:01 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update] #ユーザー編集画面が表示されるルーティング
   resources :rooms, only: [:new, :create]  #roomsのルーティング
 end
