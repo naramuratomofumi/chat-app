402c3db7e32b000d1f0ec5052ed7080086164e77
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-24 14:40:42 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
   resources :users, only: [:edit, :update] #ユーザー編集画面が表示されるルーティング
+  resources :rooms, only: [:new, :create]  #roomsのルーティング
 end
