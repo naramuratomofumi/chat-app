9e46d7b715b214ca02b6eb469e8029c24c70b307
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-24 20:38:08 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
 
   validates :content, presence: true #「content」カラムに、presence: trueを設けることで、空の場合はDBに保存しない
 end
