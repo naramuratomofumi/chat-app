bfb9f7f06e4c38d321fdd02c2b59864fb1c3e598
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-24 13:04:30 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true #「name」カラムに、presence: trueを設けることで、空の場合はDBに保存しないというバリデーションを設定
 end
