53ebe1bc3787df12b10fc5df684feb503363404a
naramuratomofumi <tomofuminaramura@gmail.com>
2021-11-24 20:19:46 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "rooms#index"
   resources :users, only: [:edit, :update] #ユーザー編集画面が表示されるルーティング
-  resources :rooms, only: [:new, :create] do  #roomsのルーティング
+  resources :rooms, only: [:new, :create, :destroy] do  #roomsのルーティング、チャットルームの削除機能を実装するために、resources :roomsにdestroyアクションのルーティングを追記
     resources :messages, only: [:index, :create] #roomsが親で、messagesが子という親子関係(ルーティングのネスト)
   end
 end
